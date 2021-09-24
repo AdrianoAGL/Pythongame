79f1d5039cbe6bc44e0df7569d289a99d8e03727
AdrianoAGL <79981335+AdrianoAGL@users.noreply.github.com>
2021-09-24 16:10:04 -0300

    Add files via upload

---
+++ joguinho.py
@@ -0,0 +1,587 @@
+pontos = 0
+
+resp = input("Digite a resposta da pergunta 1 (A/B/C/D): ")
+if resp == 'B':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 1 (A/B/C/D): ")
+    if resp == 'B':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 1 (A/B/C/D): ")
+        if resp == 'B':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 2 (A/B/C/D): ")
+if resp == 'A':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 2 (A/B/C/D): ")
+    if resp == 'A':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 2 (A/B/C/D): ")
+        if resp == 'A':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+
+resp = input("Digite a resposta da pergunta 3 (A/B/C/D): ")
+if resp == 'A':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 3 (A/B/C/D): ")
+    if resp == 'A':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 3 (A/B/C/D): ")
+        if resp == 'A':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+
+resp = input("Digite a resposta da pergunta 4 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 4 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 4 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+
+resp = input("Digite a resposta da pergunta 5 (A/B/C/D): ")
+if resp == 'C':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 5 (A/B/C/D): ")
+    if resp == 'C':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 5 (A/B/C/D): ")
+        if resp == 'C':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+
+resp = input("Digite a resposta da pergunta 6 (A/B/C/D): ")
+if resp == 'A':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 6 (A/B/C/D): ")
+    if resp == 'A':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 6 (A/B/C/D): ")
+        if resp == 'A':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+
+resp = input("Digite a resposta da pergunta 7 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 7 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 7 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 8 (A/B/C/D): ")
+if resp == 'A':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 8 (A/B/C/D): ")
+    if resp == 'A':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 8 (A/B/C/D): ")
+        if resp == 'A':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 9 (A/B/C/D): ")
+if resp == 'A':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 9 (A/B/C/D): ")
+    if resp == 'A':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 9 (A/B/C/D): ")
+        if resp == 'A':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 10 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 10 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 10 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 11 (A/B/C/D): ")
+if resp == 'C':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 11 (A/B/C/D): ")
+    if resp == 'C':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 11 (A/B/C/D): ")
+        if resp == 'C':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 12 (A/B/C/D): ")
+if resp == 'B':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 12 (A/B/C/D): ")
+    if resp == 'B':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 12 (A/B/C/D): ")
+        if resp == 'B':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 13 (A/B/C/D): ")
+if resp == 'B':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 13 (A/B/C/D): ")
+    if resp == 'B':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 13 (A/B/C/D): ")
+        if resp == 'B':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 14 (A/B/C/D): ")
+if resp == 'C':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 14 (A/B/C/D): ")
+    if resp == 'C':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 14 (A/B/C/D): ")
+        if resp == 'C':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 15 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 15 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 15 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 16 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 16 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 16 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 17 (A/B/C/D): ")
+if resp == 'A':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 17 (A/B/C/D): ")
+    if resp == 'A':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 17 (A/B/C/D): ")
+        if resp == 'A':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 18 (A/B/C/D): ")
+if resp == 'B':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 18 (A/B/C/D): ")
+    if resp == 'B':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 18 (A/B/C/D): ")
+        if resp == 'B':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 19 (A/B/C/D): ")
+if resp == 'A':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 19 (A/B/C/D): ")
+    if resp == 'A':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 19 (A/B/C/D): ")
+        if resp == 'A':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 20 (A/B/C/D): ")
+if resp == 'B':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 20 (A/B/C/D): ")
+    if resp == 'B':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 20 (A/B/C/D): ")
+        if resp == 'B':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 21 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 21 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 21 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 22 (A/B/C/D): ")
+if resp == 'A':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 22 (A/B/C/D): ")
+    if resp == 'A':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 22 (A/B/C/D): ")
+        if resp == 'A':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 23 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 23 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 23 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 24 (A/B/C/D): ")
+if resp == 'A':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 24 (A/B/C/D): ")
+    if resp == 'A':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 24 (A/B/C/D): ")
+        if resp == 'A':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 25 (A/B/C/D): ")
+if resp == 'C':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 25 (A/B/C/D): ")
+    if resp == 'C':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 25 (A/B/C/D): ")
+        if resp == 'C':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 26 (A/B/C/D): ")
+if resp == 'C':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 26 (A/B/C/D): ")
+    if resp == 'C':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 26 (A/B/C/D): ")
+        if resp == 'C':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 27 (A/B/C/D): ")
+if resp == 'B':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 27 (A/B/C/D): ")
+    if resp == 'B':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 27 (A/B/C/D): ")
+        if resp == 'B':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 28 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 28 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 28 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 29 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 29 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 29 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+resp = input("Digite a resposta da pergunta 30 (A/B/C/D): ")
+if resp == 'D':
+    pontos += 3
+    print("Certa Resposta!!")
+else:
+    print("Resposta errada tente denovo")
+    resp = input("Digite a resposta da pergunta 30 (A/B/C/D): ")
+    if resp == 'D':
+        pontos += 2
+        print("Certa Resposta!!")
+    else:
+        print("Resposta errada tente denovo")
+        resp = input("Digite a resposta da pergunta 30 (A/B/C/D): ")
+        if resp == 'D':
+            pontos += 1
+            print("Certa Resposta!!")
+        else:
+            print("Você esgotou todas as suas chances =(")
+
+print("Você fez", pontos, "pontos")
+
+if pontos >= 75:
+    print("Se é o bixão mesmo em doido")
+elif pontos == 0:
+    print("PHATETIC!!")
+elif pontos < 40:
+    print("Acerto a miseravi")
+elif pontos < 75:
+    print("Bom não é das melhores mas pelo menos é trabalho honesto")
